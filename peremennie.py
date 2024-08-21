052ed64f08e45840898f517332496a7e7fab4d73
ciberSi4 <alex.baikonur@mail.ru>
2024-08-21 21:45:32 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,8 @@
+# Переменные
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_for_one_task = Number_of_hours_spent / The_number_of_completed_DZ
+print ( "Курс:", Course_name, ", всего задач: ", The_number_of_completed_DZ,
+        ", затрачено часов: ", Number_of_hours_spent, ", среднее время выполнения -",
+        Time_for_one_task, "часа." )
